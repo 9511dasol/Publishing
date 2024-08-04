987f9f297b3a0fbaf799b10e56fed3e35b95cc89
DaSolHan <9511dasole@gmail.com>
2024-08-04 22:41:09 +0900

    first commit

---
+++ src/components/pub1/Pub1.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import "boxicons/css/boxicons.min.css";
+import "./style.css";
+export default function Pub1() {
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+  const [state, setState] = useState("Login");
+  const login = () => {
+    alert("아직 공사중입니다.");
+  };
+  const enter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      login();
+    }
+  };
+
+  return (
+    <div className="wraaper">
+      <div className="form">
+        <h1>Login</h1>
+        <div className="input-box">
+          <input
+            type="text"
+            placeholder="username"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+            onKeyDown={enter}
+          />
+          <i className="bx bxs-user"></i>
+        </div>
+        <div className="input-box">
+          <input
+            type="password"
+            placeholder="Password"
+            value={pw}
+            onChange={(e) => setPw(e.target.value)}
+            onKeyDown={enter}
+          />
+          <i className="bx bxs-lock-alt"></i>
+        </div>
+        <div className="remember-forget">
+          <label>
+            <input type="checkbox" />
+            Remember me
+          </label>
+          <a onClick={() => setState("FIND")}>Forget Password ?</a>
+        </div>
+        <button className="btn" onClick={login}>
+          Login
+        </button>
+        <div className="register-link">
+          <p>
+            Don't have a account? <a href="#">Register</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
